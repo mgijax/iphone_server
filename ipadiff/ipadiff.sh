a6bb5408d9f9dd9baa0d97e63f71817c7ae18d2c
Joel Richardson <joel.richardson@jax.org>
2026-01-20 11:14:50 -0500

    Backout accidental path change.

--- ipadiff/ipadiff.sh
+++ ipadiff/ipadiff.sh
@@ -5,8 +5,7 @@ MV=mv
 #
 REPORTSDIR=/export/gondor/ftp/pub/reports
 ARCHIVEDIR=${REPORTSDIR}/archive/iphone
-#IPHONEAPPDIR=/usr/local/mgi/proto/prototypes/iphone_app
-IPHONEAPPDIR=/home/jer/work/mgi/iphone_server
+IPHONEAPPDIR=/usr/local/mgi/proto/prototypes/iphone_app
 TMPFILE=${IPHONEAPPDIR}/htdocs/.mgiRSS.xml
 OUTPUTFILE=${IPHONEAPPDIR}/htdocs/mgiRSS.xml
 #
